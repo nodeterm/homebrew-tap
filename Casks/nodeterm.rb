492e5d03a64cf9422f66d7f7f499d540a98f50d7
nodeterm-release <release@nodeterm.dev>
2026-09-25 23:58:54 +0000

    cask: nodeterm v0.3.15

--- Casks/nodeterm.rb
+++ Casks/nodeterm.rb
@@ -1,12 +1,12 @@
 cask "nodeterm" do
-  version "0.3.14"
+  version "0.3.15"
 
   on_arm do
-    sha256 "5e9364714320fd62eb5b26c04078b5ad49762615b46b7b39cba8dfe27eb4a666"
+    sha256 "137fd2ec601444c4d7db3d8790c2bd2de587dc7372462b4564b2698f2b9d642a"
     url "https://github.com/eneskirca/nodeterm/releases/download/v#{version}/nodeterm-#{version}-arm64.dmg"
   end
   on_intel do
-    sha256 "f92eabb39d98e5eda5130087d3af6e8954e7d5a8ba9919889ad194f8152a2d8b"
+    sha256 "35308bfee4a4a21c7bf635abe2e914f02c06dbb7904ad09fef9b45a1e0d252e1"
     url "https://github.com/eneskirca/nodeterm/releases/download/v#{version}/nodeterm-#{version}.dmg"
   end
 
